ef71f0ae3a96e4844ede449e3005f469fa68e24e
Max Fechner <max.fechner@uni-muenster.de>
2019-11-07 17:17:13 +0100

    query string parsing

--- app-router.tag.js
+++ app-router.tag.js
@@ -8,17 +8,24 @@ window.customElements.define('app-router', class extends HTMLElement {
         // setTimeout(e=>this.checkHash(),100);
     }
     checkHash() {
+        let hash = document.location.hash.substr(1);
+        let queryString = {};
+        for(let pair of hash.split('&')) {
+            let kv = pair.split('=');
+            queryString[kv[0]] = kv[1];
+        }
+        // console.log('query-string',queryString);
         let routes = this.querySelectorAll('[route]');
         // console.log('children',routes, this.innerHTML);
         for (let i = 0; i < routes.length; i++) {
             let route = routes[i];
             // console.log('check',route);
-            let match = new RegExp(route.getAttribute('route')).exec(document.location.hash.substr(1));
-            console.log('match', route.getAttribute('route'), match);
+            let match = new RegExp(route.getAttribute('route')).exec(hash);
+            // console.log('match', route.getAttribute('route'), match);
             if (match)// && route.hidden==true)
                 // if (route.hasAttribute('call'))
                     if(route.routeChange) 
-                        route.routeChange({ base: match[0], wakeup: route.hidden }, ...match.slice(1));
+                        route.routeChange({ base: match[0], wakeup: route.hidden, queryString: queryString }, ...match.slice(1));
             // route[route.getAttribute('call')](...match.slice(1));
             // if (match)
             //     for (let j = 1; j < match.length; j++)
